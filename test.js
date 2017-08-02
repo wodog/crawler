6a6c813bbc526102d3ade3c4eebfcbd853f66922
wodog <536505032@qq.com>
2017-08-02 11:13:35 +0800

    to

--- test.js
+++ test.js
@@ -1,13 +1,22 @@
 const github = require('./lib/github')
+const co = require('co')
 
-github.repos.getContent({
-  owner: 'wodog',
-  repo: 'nodoc',
-  path: ''
-}).then(data => {
-  console.log(data)
+// github.repos.getContent({
+//   owner: 'wodog',
+//   repo: 'nodoc',
+//   path: ''
+// }).then(data => {
+//   console.log(data)
+// })
+
+co(function * () {
+  let members = yield github.orgs.getMembers({
+    org: 'eleme',
+    page: 1,
+    per_page: 10
+  })
+  console.log(members)
 })
-
 // const config = require('./services/user')
 // config.handler()
 
