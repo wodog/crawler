627f55ff2d893902929a36345e59d37dad503b42
wodog <536505032@qq.com>
2017-08-09 16:24:26 +0800

    debug timeout

--- services/test.js
+++ services/test.js
@@ -1,3 +1,29 @@
+const Api = require('../lib/api')
+
 exports.handler = function(event, context, callback) {
-  callback('ok')
+  callback('ok1')
+  setTimeout(function() {
+    const stats = Api.createRecord('stats', {
+      url: '20',
+      finished: false
+    })
+  }, 20 * 1000)
+  setTimeout(function() {
+    const stats = Api.createRecord('stats', {
+      url: '50',
+      finished: false
+    })
+  }, 50 * 1000)
+  setTimeout(function() {
+    const stats = Api.createRecord('stats', {
+      url: '100',
+      finished: false
+    })
+  }, 100 * 1000)
+  setTimeout(function() {
+    const stats = Api.createRecord('stats', {
+      url: '200',
+      finished: false
+    })
+  }, 200 * 1000)
 }
