3338e0d276ec1f5b09b7f310f6f6f74166891e4f
wodog <536505032@qq.com>
2017-08-17 18:00:56 +0800

    clear stats before crawler

--- services/github-repos.js
+++ services/github-repos.js
@@ -6,19 +6,21 @@ const Api = require('../lib/api')
 const config = require('../config')
 
 exports.handler = function (event, context, callback) {
-  Api.queryRecords('user').then(users => {
-    users = users.map(user => user.name)
-    for (const user of users) {
-      const data = {
-        user: user
+  Api.deleteRecords().then(() => {
+    Api.queryRecords('user').then(users => {
+      users = users.map(user => user.name)
+      for (const user of users) {
+        const data = {
+          user: user
+        }
+        if (config.debug) {
+          require('./github-repo').handler(Buffer.from(JSON.stringify(data)), null, console.log)
+        } else {
+          Api.invoke('github-repo', data)
+        }
       }
-      if (config.debug) {
-        require('./github-repo').handler(Buffer.from(JSON.stringify(data)), null, console.log)
-      } else {
-        Api.invoke('github-repo', data)
-      }
-    }
-    if (config.debug) return
-    callback(null, `执行所有用户 ${users} 的仓库`)
-  }).catch(callback)
+      if (config.debug) return
+      callback(null, `执行所有用户 ${users} 的仓库`)
+    }).catch(callback)
+  })
 }
